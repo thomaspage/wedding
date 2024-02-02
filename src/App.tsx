09cd67df4e9915db049fd72a11a09a81085f2481
Thomas Page <thomas.page@gensys.com>
2024-02-01 19:07:27 -0500

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import styled, { ThemeProvider, useTheme } from "styled-components";
+import { createTheme } from "@mui/material";
 
 function App() {
+
+  const theme = createTheme({
+
+  })
+
+  const Test = styled("div")(({theme}) => ({
+    backgroundColor: theme.palette.primary.main,
+  }))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Test className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </Test>
+    </ThemeProvider>
   );
 }
 
